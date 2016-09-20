a33ec9f4aa39cc3de478580b9cf9ba4352cbc07c
kariharanthilagakuma <kariharanthilagakuma@AA0508-26.labs.sce.carleton.ca>
2016-09-20 12:16:39 -0400

    Updated main

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,12 +4,29 @@ import java.util.ArrayList;
 public class AddressBook {
 	ArrayList<BuddyInfo> Collect = new ArrayList<BuddyInfo>();
 	
-	public void addBuddy(String Name, String Address, int Phone_Number){
-		BuddyInfo Friend = new BuddyInfo(Name, Address, Phone_Number);
+	public AddressBook(){
+		ArrayList<BuddyInfo> Collection = new ArrayList<BuddyInfo>();
+		Collect = Collection;
+	}
+	
+	public void addBuddy(BuddyInfo Friend){
 		Collect.add(Friend);
 	}
 	
 	public void removeBuddy(BuddyInfo Friend){
 		Collect.remove(Friend);
+	}
+	
+	public static void main(String[] args) {
+		AddressBook Friends = new AddressBook();
+		BuddyInfo Tom = new BuddyInfo("Tom", "Carleton", 61300000);
+		BuddyInfo Jill = new BuddyInfo("Jill", "Carleton", 61300001);
+		BuddyInfo Alex = new BuddyInfo("Alex", "Carleton", 61300002);
+		Friends.addBuddy(Tom);
+		Friends.addBuddy(Jill);
+		Friends.addBuddy(Alex);
+		Friends.removeBuddy(Tom);
+		Friends.removeBuddy(Jill);
+		Friends.removeBuddy(Alex);
 	}
 }
